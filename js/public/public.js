3627177a93a3bc39a97beef4c48ba37b69d152ac
wenxu <pigyaa@126.com>
2016-04-18 17:03:45 +0800

    模拟登陆

--- js/public/public.js
+++ js/public/public.js
@@ -74,7 +74,7 @@ function getQueryString(name) {
 //模拟登陆
 $.ajax({
     dataType: "jsonp",
-    url:"http://172.31.10.52/usersystem/login/memberLogin/v1?id=123",
+    url:"http://172.31.10.52/usersystem/login/memberLogin/v1",
     data: {account:"sunxy005",password:"123"},
     success:function(data){
 
