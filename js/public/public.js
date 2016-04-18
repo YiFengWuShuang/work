7fa4dcf78c5abf44d2a19b30bed26a765f9a992f
wenxu <pigyaa@126.com>
2016-04-18 16:48:28 +0800

    模拟登陆

--- js/public/public.js
+++ js/public/public.js
@@ -78,7 +78,6 @@ $.ajax({
     url:"http://172.31.10.52/usersystem/login/memberLogin/v1",
     dataType: "jsonp",
 	jsonp: "callback",
-	jsonpCallback:"flightHandler",
     data: {account:"sunxy005",password:"123"},
     success:function(data){
     	data = data || {};
