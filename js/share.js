68fea1f8603d73267fd0a5311c96c5bc0853d060
wenxu <pigyaa@126.com>
2016-04-21 11:38:22 +0800

    分享js

--- js/share.js
+++ js/share.js
@@ -1,15 +1,55 @@
 define(function(require, exports, module){
 	
 	function ShareFn(opts){
+		this.cssname = opts.cssname;
+		this.containers = opts.containers;
+		this.childrens = opts.childrens;
 		this.init();
 	}
 
 	ShareFn.prototype = {
 		init: function(){
-			alert(1)
+			this.evens();
+		},
+		getCssNames: function(){
+			var that = this, names = [];
+			for(var i=0, L=that.cssname.length; i<L; i++){
+				names.push(that.cssname.eq(i).attr('data-cssname'));
+			}
+			return names;
+		},
+		createLink: function(){
+			var that = this, links = '', names = that.getCssNames();
+			for(var i=0, L=that.cssname.length; i<L; i++){
+				links += '<link rel="stylesheet" href="../css/'+ names[i] +'.css">';
+			}
+			return links;
+		},
+		fullCon: function(){
+			return this.containers.html();
+		},
+		filterCon: function(){
+			var that = this,
+				L = that.childrens.length,
+				_containers = that.containers.html(),
+				c_html = '';
+			for(var i=0; i<L; i++){
+				$(that.childrens[i]).each(function(){
+					c_html = $(this)[0].outerHTML;
+					_containers = _containers.replace(c_html,'');
+				})
+			}
+			return _containers;
+		},
+		evens: function(){
+			var that = this, shareCon = '', L = that.childrens.length;
+			shareCon = that.createLink() + ( (L==0) ? that.fullCon() : that.filterCon() );
+			return shareCon;
 		}
 	}
 
-	module.exports = shareFn
+	module.exports = function(opts){
+		return new ShareFn(opts);
+	};
 	
 });
