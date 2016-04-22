89593d7bde43614ace95a320125afff3d38544f3
wenxu <pigyaa@126.com>
2016-04-22 11:34:31 +0800

    答交js

--- js/payInfo.js
+++ js/payInfo.js
@@ -11,7 +11,7 @@ define(function(require, exports, module){
                 //dataType: "json",
                 url:config.serviceUrl,
                 data: {
-			        "param": '{ "token":"9b14aff650e129870793d4eabd944cb5", "serviceId":"B03_getPurchaseOrderInfo", "secretNumber":"f07e773c7c66c684f5c11a26225fa88e", "poId":"100001000000001", "companyId":"10000001", "commonParam":{ "mobileSysVersion":"1", "sourcePage":"1", "mobileModel":"1", "sourceSystem":"1", "interfaceVersion":"1", "dataSource":"1" } }'
+			        "param": '{ "token":"97dd3f9c87b5b8b937392cb1f6048630", "serviceId":"B03_getPurchaseOrderInfo", "secretNumber":"0b7619a54e80f9b5b8f96134b2ba9f6a", "poId":"100001000000001", "companyId":"10000001", "commonParam":{ "mobileSysVersion":"1", "sourcePage":"1", "mobileModel":"1", "sourceSystem":"1", "interfaceVersion":"1", "dataSource":"1" } }'
 			    },
                 success:function(data){
                 	data = data || {};
