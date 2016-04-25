92524c7bd0b79375e54ed1cfdee4dc774a085507
wenxu <pigyaa@126.com>
2016-04-25 17:38:15 +0800

    站内通知

--- js/msgDetail.js
+++ js/msgDetail.js
@@ -11,7 +11,7 @@ define(function(require, exports, module){
                 dataType: "json",
                 url:'http://172.31.10.155:19890/oss/notify/api',
                 data:{
-                	"param":JSON.stringify({ "params": { "content": { "header": { "module": "", "key": "", "operator": "" }, "body": { "method": "queryMessage", "data": { "UserId":1, "Id": 10, "pageInfo":{} } } } } })
+                	"param":{"userId":123, "Id":456}
                 },
                 success:function(data){
                 	data = data || {};
