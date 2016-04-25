245264854c6dcc85ebd8f5acf90c0bd8d6cd4020
wenxu <pigyaa@126.com>
2016-04-25 21:11:52 +0800

    站内通知

--- js/msgDetail.js
+++ js/msgDetail.js
@@ -8,14 +8,14 @@ define(function(require, exports, module){
 			var result='', userId = getQueryString('userId'), id = getQueryString('id'), time = getQueryString('time') || '';
 			$.ajax({
 				type:"POST",
-                dataType: "json",
+                //dataType: "json",
                 url:'http://172.31.10.155:19890/oss/notify/api',
                 data:{
-                	"params": '{ "content": { "header": { "module": "", "key": "", "operator": "" }, "body": { "method": "queryMessage", "data": { "UserId":1, "Id": 10, "pageInfo":{}}}}}'
+                	"params": '{ "content": { "header": { "module": "","key": "","operator": "" }, "body": { "method": "queryMessage", "data": { "UserId":1, "Id": 10, "pageInfo":{}}}}}'
                 },
                 success:function(data){
                 	data = data || {};
-                	if(data){
+                	if(data.errorCode=='0'){
                 		var _msg = data.dataSet.data;
                 		result	+='<div class="itemHead">'
 								+'	<h2>'+ _msg.detail[0].UserName +'</h2>'
@@ -25,6 +25,8 @@ define(function(require, exports, module){
 								+'	<h3>'+ _msg.detail[0].Title +'</h3>'
 								+'	<section>'+ _msg.detail[0].Content +'</section>'
 								+'</div>'
+                	}else{
+                		console.log(data.errorMsg);
                 	}
                 },
                 error:function(){
