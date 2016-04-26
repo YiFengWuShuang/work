5a72bc180066c716ff45c470758866408c300910
wenxu <pigyaa@126.com>
2016-04-26 17:45:42 +0800

    修改js

--- js/payInfo2.js
+++ js/payInfo2.js
@@ -11,7 +11,7 @@ define(function(require, exports, module){
                 async: false,
                 url:config.serviceUrl,
                 data: {
-			        "param": '{"secretNumber":"","token":"","serviceId":"B01_findFileList","companyId":"10000001","fileSource":"1","searchType":"1","id":"100001000000001","docType":"10"}'
+			        "param": '{"secretNumber":"4bef6eefabd0ef34ab42562e1b956852","token":"c5ef4cca77e9a7821ea5df0da650f07e","serviceId":"B01_findFileList","companyId":"10000001","fileSource":"1","searchType":"1","id":"100001000000001","docType":"10"}'
 			    },
                 success:function(data){
                 	data = data || {};
@@ -32,7 +32,7 @@ define(function(require, exports, module){
 				type:"POST",
                 url:config.serviceUrl,
                 data: {
-			        "param": '{ "token":"9b14aff650e129870793d4eabd944cb5", "serviceId":"B03_getPurchaseOrderInfo", "secretNumber":"f07e773c7c66c684f5c11a26225fa88e", "poId":"100001000000001", "companyId":"10000001", "commonParam":{ "mobileSysVersion":"1", "sourcePage":"1", "mobileModel":"1", "sourceSystem":"1", "interfaceVersion":"1", "dataSource":"1" } }'
+			        "param": '{ "token":"c5ef4cca77e9a7821ea5df0da650f07e", "serviceId":"B03_getPurchaseOrderInfo", "secretNumber":"4bef6eefabd0ef34ab42562e1b956852", "poId":"100001000000110", "companyId":"10000001", "commonParam":{ "mobileSysVersion":"1", "sourcePage":"1", "mobileModel":"1", "sourceSystem":"1", "interfaceVersion":"1", "dataSource":"1" } }'
 			    },
                 success:function(data){
                 	data = data || {};
