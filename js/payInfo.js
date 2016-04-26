92ad564bfdd60d79bca047160b372e91de29cdfe
wenxu <pigyaa@126.com>
2016-04-26 14:43:49 +0800

    修改js

--- js/payInfo.js
+++ js/payInfo.js
@@ -7,10 +7,10 @@ define(function(require, exports, module){
 		//支付信息
 		payInfo: function(){
 			var that = this, html = '';
-			var params = {"params":{ "token":_vParams.token, "serviceId":"B03_getPurchaseOrderInfo", "secretNumber":_vParams.secretNumber, "poId":_vParams.poId, "companyId":_vParams.companyId, "commonParam":{ "mobileSysVersion":"1", "sourcePage":"1", "mobileModel":"1", "sourceSystem":"1", "interfaceVersion":"1", "dataSource":"1" }}};
+			var params = {"params":{ "token":"2827958e61007446320db857d994e31a", "serviceId":"B03_getPurchaseOrderInfo", "secretNumber":"f9b79a20b1007a3ea46daae1b863bb72", "poId":"1000000000028", "companyId":"10000021", "commonParam":{ "mobileSysVersion":"1", "sourcePage":"1", "mobileModel":"1", "sourceSystem":"1", "interfaceVersion":"1", "dataSource":"1" }}};
 			$.ajax({
 				type:"POST",
-                //dataType: "json",
+                dataType: "json",
                 async: false,
                 url:config.serviceUrl,
 			    data:JSON.stringify(params),
