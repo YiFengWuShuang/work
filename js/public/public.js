ff517b54c543adafaa58402bb363e3897cddb420
wenxu <pigyaa@126.com>
2016-04-27 11:57:05 +0800

    修改js

--- js/public/public.js
+++ js/public/public.js
@@ -34,7 +34,7 @@ var fnTip = {
 		},time);
 	},
 	loading: function(){
-		var loading = '.loader-inner{ background: rgba(0,0,0,.7); text-align: center;}.ball-clip-rotate>div{background-color:#fff;width:2rem;height:2rem;border-radius:100%;margin-top:.6rem;-webkit-animation-fill-mode:both;animation-fill-mode:both;border:2px solid #fff;border-bottom-color:transparent;background:transparent!important;display:inline-block;-webkit-animation:rotate .75s 0s linear infinite;animation:rotate .75s 0s linear infinite}@keyframes rotate{0%{-webkit-transform:rotate(0deg) scale(1);transform:rotate(0deg) scale(1)}50%{-webkit-transform:rotate(180deg) scale(0.6);transform:rotate(180deg) scale(0.6)}100%{-webkit-transform:rotate(360deg) scale(1);transform:rotate(360deg) scale(1)}}<div class="loader-inner ball-clip-rotate"><div></div></div>';
+		var loading = '<style>.loader-inner{ background: rgba(0,0,0,.7); text-align: center;}.ball-clip-rotate>div{background-color:#fff;width:2rem;height:2rem;border-radius:100%;margin-top:.6rem;-webkit-animation-fill-mode:both;animation-fill-mode:both;border:2px solid #fff;border-bottom-color:transparent;background:transparent!important;display:inline-block;-webkit-animation:rotate .75s 0s linear infinite;animation:rotate .75s 0s linear infinite}@keyframes rotate{0%{-webkit-transform:rotate(0deg) scale(1);transform:rotate(0deg) scale(1)}50%{-webkit-transform:rotate(180deg) scale(0.6);transform:rotate(180deg) scale(0.6)}100%{-webkit-transform:rotate(360deg) scale(1);transform:rotate(360deg) scale(1)}}</style><div class="loader-inner ball-clip-rotate"><div></div></div>';
 		$body.append(loading);
 	},
 	hideLoading: function(){
