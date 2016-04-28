23541fef1193f2acc4bcfbaad8d213596959d789
wenxu <pigyaa@126.com>
2016-04-28 15:39:09 +0800

    修改js

--- js/payInfo.js
+++ js/payInfo.js
@@ -1,26 +1,27 @@
 define(function(require, exports, module){
 	var _vParams = JSON.parse(decodeURI(getQueryString('param')));
+	var storage = window.localStorage;
 	var lists = {
 		init: function(){
 			var that = this;
-			that.logisticsType = '';
-			that.paymentType = '';
-			that.invoiceType = '';
-			requestFn("B02_LogisticsType",function(data){
-				if(data.errorCode=='0'){
-					that.logisticsType = data.dataSet.data.detail
-				}
-			});
-			requestFn("B02_PaymentType",function(data){
-				if(data.errorCode=='0'){
-					that.paymentType = data.dataSet.data.detail
-				}
-			});
-			requestFn("B02_InvoiceType",function(data){
-				if(data.errorCode=='0'){
-					that.invoiceType = data.dataSet.data.detail
-				}
-			});
+			
+			//缓存枚举数组
+			if(storage.getItem('B02_LogisticsType')==null){
+				requestFn("B02_LogisticsType",function(data){
+					if(data.errorCode=='0'){
+						that.logisticsType = data.dataSet.data.detail;
+						storage.setItem('B02_LogisticsType',JSON.stringify(data.dataSet.data.detail));
+					}
+				});				
+			}
+			if(storage.getItem('B02_InvoiceType')==null){
+				requestFn("B02_InvoiceType",function(data){
+					if(data.errorCode=='0'){
+						that.invoiceType = data.dataSet.data.detail;
+						storage.setItem('B02_InvoiceType',JSON.stringify(data.dataSet.data.detail));
+					}
+				});
+			}
 			that.start();
 			fnTip.hideLoading();
 		},
@@ -45,11 +46,10 @@ define(function(require, exports, module){
                 	if(data.success){
                 		var infos = data.purchaseOrderInfo;
                 		html += '<li><span>交易条件：</span><p>'+ infos.conditionName +'</p></li>'
-								+'<li><span>物流方式：</span><p>'+ enumFn(that.logisticsType,infos.logisticsType) +((infos.logisticsType=='3') ? '（自提点：'+ infos.address +'）':'')+'</p></li>'
+								+'<li><span>物流方式：</span><p>'+ enumFn(JSON.parse(storage.getItem('B02_LogisticsType')),infos.logisticsType) +((infos.logisticsType=='3') ? '（自提点：'+ infos.address +'）':'')+'</p></li>'
 								+'<li><span>收货地址：</span><p>'+ infos.address +'；<br>电话：'+ infos.mobile +'</p></li>'
 								+'<li><span>付款条件：</span><p>'+ infos.payWayName +'</p></li>'
-								+'<li><span>支付方式：</span><p>'+ enumFn(that.paymentType,infos.paymentType) +'</p></li>'
-								+'<li><span>发票类型：</span><p>'+ enumFn(that.invoiceType,infos.invoiceType) +'</p></li>'
+								+'<li><span>发票类型：</span><p>'+ enumFn(JSON.parse(storage.getItem('B02_InvoiceType')),infos.invoiceType) +'</p></li>'
 								+'<li><span>发票抬头：</span><p>'+ infos.invoiceHeader +'</p></li>'
 								+'<li><span>发票类容：</span><p>'+ infos.invoiceContent +'</p></li>'
                 	}else{
