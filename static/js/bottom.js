831a648c48ae56f8af6dbc169c79f9788380390b
wenxu <pigyaa@126.com>
2016-05-06 17:41:34 +0800

    增加js

---
+++ static/js/bottom.js
@@ -0,0 +1,58 @@
+/*
+offItems  不显示的项，类型是数组类型
+*/
+
+function connectWebViewJavascriptBridge(callback) {
+    if (window.WebViewJavascriptBridge) {
+        callback(WebViewJavascriptBridge)
+    } else {
+        document.addEventListener(
+            'WebViewJavascriptBridgeReady'
+            , function() {
+                callback(WebViewJavascriptBridge)
+            },
+            false
+        );
+    }
+}
+
+       
+function bottomBar(offItems,mobile,memberId){
+	var items = ['mobile','share','im'], sOffItems = offItems.join(',');
+	var bottomDom = '<div id="jBottom" class="m-bottom-wrap clearfix"><div class="bottom-side">';
+	items.forEach(function(val){
+		if(sOffItems.indexOf(val)=='-1'){
+			bottomDom+='<a href="javascript:;" id="btn-'+val+'" class="'+ val +'"></a>'
+		}
+	})
+	bottomDom+='</div><a href="javascript:;" class="bottom-btn">完成</a></div>'
+	$body.append(bottomDom);
+	btnEvens(mobile,memberId);
+}
+
+function btnEvens(mobile,memberId){
+	$body.on('click','.bottom-side a', function(){
+		var _this = $(this);
+		if(_this.is('#btn-mobile')){
+			btmBtnMobile(mobile)
+		}
+		if(_this.is('#btn-share')){
+			btmBtnShare()
+		}
+		if(_this.is('#btn-im')){
+			btmBtnIm(memberId)
+		}
+	})
+}
+
+function btmBtnMobile(mobile){
+	window.WebViewJavascriptBridge.callHandler( "call", {"param":mobile}, function(responseData) {});
+}
+
+function btmBtnShare(){
+
+}
+
+function btmBtnIm(memberId){
+
+}
