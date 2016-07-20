7fbea2bb9cdec39f655fba3b6bac10e97bfd80ef
xuyifeng <dkcuiui@163.com>
2016-07-20 10:57:59 +0800

    分享页

--- static/js/share.js
+++ static/js/share.js
@@ -1,7 +1,6 @@
 /*
 销售订单
 */
-var _vParams = JSON.parse(decodeURI(getQueryString('param')));
 var container = $('.contarin');
 var $platformCurrencyList;
 var $currencySymbol = '';
@@ -9,12 +8,13 @@ var $priceDecimalNum = '';
 var $amountDecimalNum = '';
 var $fileData;
 var orderReviseInfoCon = $('#orderReviseInfoCon');
-var _reg = /^(\s|\S)+(jpg|jpeg|png|gif|bmp|JPG|JPEG|PNG|GIF|BMP)+$/;
 var salesDetail = function(){
 	this.init();
 }
-salesDetail.prototype = {
-	init: function(){
+salesDetail.prototype = 
+{
+	init: function()
+	{
 		var that = this;
 		that.commonParam = JSON.stringify(commonParam());
 		that.tokens = '"token":"'+ _vParams.token +'","secretNumber":"'+ _vParams.secretNumber +'"';
@@ -46,6 +46,8 @@ salesDetail.prototype = {
 		that.prodsInfo();
 		that.otherCostList();
 		that.start();
+		that.payInfo();
+		that.remark();
 	},
 	//基础信息
 	orderBaseInfo: function(){
@@ -88,6 +90,24 @@ salesDetail.prototype = {
 	//产品明细
 	prodsInfo: function(){
 		var that = this, html = '';
+	    //加载单身附件
+	    function getObFileList(line){
+	        line.fileList = [];
+
+	        GetAJAXData('POST',{"serviceId":"B01_findFileList", "docType":12, "companyId":_vParams.companyId, "searchType":2, "id":line.id, "token":_vParams.token, "secretNumber":_vParams.secretNumber,"commonParam":commonParam()},function(data){
+				if(data.success){
+					data.fileList.forEach(function(v){
+	                    line.fileList.push({
+	                        "id": v.id,
+	                        "fileName":v.fileName,
+	                        "fileSize":v.fileSize,
+	                        "fileUrl": v.fileUrl,
+	                        "lineNo": v.lineNo
+	                    });
+	                });
+				}
+			});	
+	    }
 		$.ajax({
 			type:"POST",
 			async:false,
@@ -118,10 +138,19 @@ salesDetail.prototype = {
 							+'		<li class="subtotal"><span>小计：</span><b>'+ $currencySymbol + formatMoney((prodInfos[i].valuationQty*prodInfos[i].taxPrice),$amountDecimalNum) +'</b></li>'//bug解决后改回prodInfos[i].taxLineTotal
 							+'	</ul>'
 							+'</div>'
-						that.totals+=Number(prodInfos[i].taxLineTotal);
+						that.totals+=parseFloat(prodInfos[i].taxLineTotal);
+						getObFileList(prodInfos[i]);
             		}
             		that.load = true;
             		$('#prodListInfo').html(html);
+            		prodInfos.forEach(function(line,idx){
+						var fileHTML = '<p>'
+						line.fileList.forEach(function(val){
+							fileHTML += '<a href="'+ val.fileUrl +'"><i class=i-'+ (_reg.test(val.fileName) ? "image" : "word") +'></i>'+ val.fileName +'</a>'
+						})
+						fileHTML += '</p>'
+						$('#prodListInfo').find('.files').eq(idx).html('<span>附件：</span>'+fileHTML).show();
+					})        
             	}else{
             		fnTip.hideLoading();
             		container.show().html('<p style="line-height:2rem; text-align:center">'+ data.errorMsg +'</p>')
@@ -146,11 +175,11 @@ salesDetail.prototype = {
             		html = '<h2 class="m-title">其他费用</h2><div class="item-wrap"><ul>';
             		for(var i=0, len=otherCostList.length; i<len; i++){
             			html+='<li><span>'+ otherCostList[i].costName +'：</span><b>'+ $currencySymbol + formatMoney(otherCostList[i].costAmount,$amountDecimalNum) +'</b></li>'
-            			subtotal += Number(otherCostList[i].costAmount);
+            			subtotal += parseFloat(otherCostList[i].costAmount);
             		}
             		html+='<li class="subtotal"><span>小计：</span><b>'+ $currencySymbol + formatMoney(that.orderInfo.vOtherCostTotal,$amountDecimalNum) +'</b></li>'
             		html+='</ul></div>';
-            		that.totals+=Number(subtotal);
+            		that.totals+=parseFloat(subtotal);
             		$('#otherCost').html(html);
             	}
             }
@@ -162,7 +191,7 @@ salesDetail.prototype = {
 
 
 		//单头附件
-		var fileParam = { "token":_vParams.token, "secretNumber":_vParams.secretNumber,"serviceId":"B01_findFileList", "companyId":that.orderInfo.companyId, "id":that.orderInfo.id, "commonParam":commonParam(), "docType":"12","fileSource":1,"searchType":1};//searchType查询类型1单头2单身
+		var fileParam = { "token":_vParams.token, "secretNumber":_vParams.secretNumber,"serviceId":"B01_findFileList", "companyId":_vParams.companyId, "id":that.orderInfo.id, "commonParam":commonParam(), "docType":"12","searchType":1};//searchType查询类型1单头2单身
 		GetAJAXData('POST',fileParam,function(fileData){
 			if(fileData.success){
 				$fileData = fileData;
@@ -174,32 +203,42 @@ salesDetail.prototype = {
 		if(that.load){
 			//status 1：待出货，2：部分出货
 			if(that.status==1||that.status==2){
-				bottomBar(['share'],that.memberId,'','出货');
+				bottomBar([''],that.memberId,'','出货');
 			}else{
 				bottomBar(['share'],that.memberId,true);
 			}
 		}
 
-		//
-		container.on('click','a.item-link',function(){
-			var _this = $(this), name = _this.attr('name'), scrollTop = $body.scrollTop();
-			switch(name){
-				case 'payInfo':
-					orderReviseInfoCon.html(that.payInfo(scrollTop));
-					break;
-				case 'remark':
-					orderReviseInfoCon.html(that.remark(scrollTop));
-					break;
-			}
-			$body.scrollTop(0);
-			container.addClass('contarinEdit');
-			$('#jBottom').addClass('m-bottom-hide');
-		}).on('click','.btn-wrap .btnB',function(){
-			var _this = $(this), scrollTop = _this.attr('data-scrollTop');
-			container.removeClass('contarinEdit');
-			$('#jBottom').removeClass('m-bottom-hide');
-			setTimeout(function(){$body.scrollTop(scrollTop)},200);
-		})
+		//点击分享
+		var btn = document.getElementById('btn-share');
+		btn.onclick = function(h){
+			h=$('#orderBaseInfoCon').html()
+			console.log(h)
+			return h;
+		}
+
+
+		// console.log(btn)
+		
+		// container.on('click','a.item-link',function(){
+		// 	var _this = $(this), name = _this.attr('name'), scrollTop = $body.scrollTop();
+		// 	switch(name){
+		// 		case 'payInfo':
+		// 			orderReviseInfoCon.html(that.payInfo(scrollTop));
+		// 			break;
+		// 		case 'remark':
+		// 			orderReviseInfoCon.html(that.remark(scrollTop));
+		// 			break;
+		// 	}
+		// 	$body.scrollTop(0);
+		// 	container.addClass('contarinEdit');
+		// 	$('#jBottom').addClass('m-bottom-hide');
+		// }).on('click','.btn-wrap .btnB',function(){
+		// 	var _this = $(this), scrollTop = _this.attr('data-scrollTop');
+		// 	container.removeClass('contarinEdit');
+		// 	$('#jBottom').removeClass('m-bottom-hide');
+		// 	setTimeout(function(){$body.scrollTop(scrollTop)},200);
+		// })
 
 		$body.on('click','.bottom-btn-confirm',function(){
 			//跳转至出货单新建
@@ -212,12 +251,15 @@ salesDetail.prototype = {
 			}
 		})
 	},
-	payInfo: function(scrollTop){
-		var that = this, infos = that.orderInfo;
 
-		var html = '<ul class="payInfoList">'
+	payInfo: function(scrollTop){
+		var that = this, infos = that.orderInfo, html='';
+
+	 html +='<h2 class="m-title">物流和发票信息</h2>' 
+	 		+'<div class="item-wrap">'
+			+'<ul>'
 			+'<li><span>交易条件：</span><p>'+ infos.conditionName +'</p></li>'
-			+'<li><span>物流方式：</span><p>'+ enumFn(that.logisticsType,infos.logisticsType) +((infos.logisticsType=='3') ? '（自提点：'+ infos.address +'）':'')+'</p></li>'
+			+'<li><span>物流方式：</span><p>'+ enumFn(that.logisticsType,infos.logisticsType) + ((infos.logisticsType!=3)?'（物流商名称：'+ infos.logisticsName +'）':'') +'</p></li>'
 			+'<li><span>'+ ((infos.logisticsType==3) ? '自提点' : '收货地址') +'：</span><p>'+ infos.provinceName + infos.cityName + infos.districtName + infos.address + '<br>收货人：'+ infos.contactPerson +'，电话：'+ infos.mobile +'</p></li>'
 			+'<li><span>付款条件：</span><p>'+ infos.payWayName +'</p></li>'
 			if(infos.invoice==1){
@@ -227,25 +269,39 @@ salesDetail.prototype = {
 					+'<li><span>发票抬头：</span><p>'+ infos.invoiceHeader +'</p></li>'
 					+'<li><span>发票类容：</span><p>'+ infos.invoiceContent +'</p></li>'			
 			}
+
 			html+='</ul>'
-			+'<div class="btn-wrap"><a href="javascript:;" class="btnB" data-scrollTop="'+scrollTop+'">返回</a></div>'
-		return html;
+			+'</div>'
+			// +'<div class="btn-wrap"><a href="javascript:;" class="btnB" data-scrollTop="'+scrollTop+'">返回</a></div>'
+		$('#payInfo').html(html)
 	},
 	remark: function(scrollTop){
 		var that = this;
-		var html = '<div class="remarks-wrap"><div id="provisions" class="item-wrap clause">'
+		var html ='<h2 class="m-title">补充条款、备注和附件</h2>'
+				 +'<div class="remarks-wrap"><div id="provisions" class="item-wrap clause">'
 				 +'	<h2>补充条款：</h2>'
 				 +'	<p>'+ that.orderInfo.agreement +'</p>'
 				 +'</div>'
 				 +'<div id="taRemarks" class="item-wrap taRemarks">'
-				 +'	<h2>备注信息：</h2>'
+				 +'	<h2>客户备注：</h2>'
 				 +'	<p>'+ that.orderInfo.remark +'</p>'
 				 +'</div>'
+				 +'<div id="vRemarks" class="item-wrap taRemarks">'
+				 +'	<h2>本方备注：</h2>'
+				 +'	<p>'+ that.orderInfo.vRemark +'</p>'
+				 +'</div>'				 
 				 +'<div id="files" class="item-wrap attachment">'
 				 +'	<h2>订单附件：</h2>'
-				 +'</div>'
-				 +'</div></div><div class="btn-wrap"><a href="javascript:;" id="saveRemark" class="btnB" data-scrollTop="'+scrollTop+'">返回</a></div>'
-		return html;
+		if($fileData.fileList.length==0){
+			html+='<p><b>0个附件</b></p>'
+		}
+		for(var i=0; i<$fileData.fileList.length;i++){
+			html+='<p><a href="'+ $fileData.fileList[i].fileUrl +'"><i class=i-'+ (_reg.test($fileData.fileList[i].fileName) ? "image" : "word") +'></i>'+ $fileData.fileList[i].fileName +'</a></p>'
+		}
+			// html +='</div>'
+			// 	 +'</div></div><div class="btn-wrap"><a href="javascript:;" id="saveRemark" class="btnB" data-scrollTop="'+scrollTop+'">返回</a></div>'
+		// return html;
+		$('#remark').html(html)
 	},
 	popup: function(type, title, content, closeCallBack, okCallBack){
 		new Popup({
@@ -259,3 +315,5 @@ salesDetail.prototype = {
 		});
 	}
 }
+
+// console.log(h);
